f1a5ecd2dd1d09da8a423665cc321f1a0c5ac81a
Eleonora2502 <ellombardo@outlook.it>
2025-04-08 18:37:18 +0200

    modifiche frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,30 @@ import Signup from './pages/Signup';
 import TripDetails from './pages/TripDetails';
 
 
+import SlotsSignIn from './pages/login1.js';
+
+import './styles/App.css'; // se hai uno stile globale
+
+
 function App() {
   return (
     <AuthProvider>
-      <Routes>
+      
+  
+
+       <Routes>
+
+       
+        <Route path="/login" element={<SlotsSignIn />} />
+        {/* altre route */}
+      
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/trip-details" element={<TripDetails />} />
       </Routes>
+     
     </AuthProvider>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ExpenseItem from '../components/ExpenseItem';
+import '../styles/home.css';
+
+
+import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import backgroundImg from '../assets/viaggi-di-gruppo.jpg';
+
 
 const Home = () => {
 
     return (
         <>
-            <h1>Home</h1>
+             <div>
+      <ResponsiveAppBar />
+
+      {/* Immagine di sfondo con testo sopra */}
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '90vh',
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <h1
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+            textShadow: '1px 1px 5px rgba(0,0,0,0.8)',
+          }}
+        >
+          Organizza il tuo viaggio insieme ai tuoi amici
+        </h1>
+      </div>
+    </div>
+  
             <a href='/login'>Accedi</a>
             <a href='/dashboard'>Dashboard</a>
             <button>{<ExpenseItem />}</button>
         </>
+        
     );
 };
 
